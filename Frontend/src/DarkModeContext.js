62402c5706b2a10db4960394589594e0b68c2686
faresSultan <fares.sultan9@gmail.com>
2024-09-23 22:20:07 +0300

    DarkModeContext

--- Frontend/src/DarkModeContext.js
+++ Frontend/src/DarkModeContext.js
@@ -1,12 +1,12 @@
 import React, { createContext, useState, useEffect } from 'react';
 
-// Create the context
+
 export const DarkModeContext = createContext();
 
 export const DarkModeProvider = ({ children }) => {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
-  // Effect to apply dark mode class to the document's body element
+
   useEffect(() => {
     if (isDarkMode) {
       document.documentElement.classList.add('dark');
@@ -15,7 +15,7 @@ export const DarkModeProvider = ({ children }) => {
     }
   }, [isDarkMode]);
 
-  // Toggle dark mode
+  
   const toggleDarkMode = () => {
     setIsDarkMode(!isDarkMode);
   };
--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { DarkModeProvider } from './DarkModeContext'; // Import DarkModeProvider
+import { DarkModeProvider } from './DarkModeContext'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <DarkModeProvider> {/* Wrap the app with DarkModeProvider */}
+    <DarkModeProvider> 
       <App />
     </DarkModeProvider>
   </React.StrictMode>
