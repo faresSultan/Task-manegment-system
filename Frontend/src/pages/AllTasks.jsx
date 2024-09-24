6f84c4d0b20bf9633a4809dc128c35ee24e7cdf5
faresSultan <fares.sultan9@gmail.com>
2024-09-24 18:26:20 +0300

    All Tasks page

---
+++ Frontend/src/pages/AllTasks.jsx
@@ -0,0 +1,23 @@
+
+import React, { useState } from 'react';
+import Cards from '../components/Home/cards';
+import { IoIosAddCircleOutline } from "react-icons/io";
+import InputData from '../components/Home/InputData';
+const Alltasks = () => {
+  const [InputDiv,setInputDiv] = useState("hidden");
+  return (
+    <>
+        <div>
+            <div className='w-full flex justify-end px-4 py-2'>
+                <button onClick={() =>setInputDiv("fixed")}><IoIosAddCircleOutline className=' text-3xl text-gray-900 dark:text-gray-200 hover:scale-150 transition-all duration-300' /></button>
+            </div>
+            <Cards home = {"true"} InputDiv = {InputDiv} setInputDiv = {setInputDiv} />
+        </div>
+
+        <InputData InputDiv = {InputDiv} setInputDiv = {setInputDiv} />
+    </>
+   
+  );
+}
+
+export default Alltasks;
