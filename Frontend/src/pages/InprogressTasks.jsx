c20204ec9d09a6b397169111a987c7795706bfd4
faresSultan <fares.sultan9@gmail.com>
2024-09-27 20:07:35 +0300

    fetching inprogress tasks

--- Frontend/src/pages/InprogressTasks.jsx
+++ Frontend/src/pages/InprogressTasks.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import axios from 'axios';
+import Cards from '../components/cards';
+import React, { useEffect, useState } from 'react';
 const InprogressTasks = () => {
+
+  const [Data,setData]=useState([]);
+ 
+  const headers= {id:localStorage.getItem("id"), authorization: `Bearer ${localStorage.getItem("token")}`}
+  useEffect(()=>{
+    const fetch = async (filter)=>{
+      const response= await axios.get(`http://localhost:8080/api/v1/tasks?filter=${filter}`, {headers,});
+      
+      setData(response.data.tasks);
+    };
+    fetch("incomplete"); 
+  },[])
   return (
-   <div>
-    progress
-   </div>
+    <div>
+    <Cards  home={"false"} data={Data}/>
+    </div>
   );
 }
 
