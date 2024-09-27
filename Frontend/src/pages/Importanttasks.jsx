6fa87ea248249048804294d24b8f62f7d0202c4f
faresSultan <fares.sultan9@gmail.com>
2024-09-27 20:10:25 +0300

    implementing important tasks page

--- Frontend/src/pages/Importanttasks.jsx
+++ Frontend/src/pages/Importanttasks.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Sidebar from '../components/Sidebar';
 import Cards from '../components/cards';
+import axios from 'axios';
 const ImportantTasks = () => {
+
+  const [Data,setData]=useState([]);
+ 
+  const headers= {id:localStorage.getItem("id"), authorization: `Bearer ${localStorage.getItem("token")}`}
+  useEffect(()=>{
+    const fetch = async (filter)=>{
+      const response= await axios.get(`http://localhost:8080/api/v1/tasks?filter=${filter}`, {headers,});
+      
+      setData(response.data.tasks);
+    };
+    fetch("important"); 
+  },[])
   return (
    <div>
-        <Cards home={false} />
+        <Cards home={false} data={Data} />
    </div>
   );
 }
