75b108460a0b5d06ad075f2afc0b8d4a4ca91dc6
faresSultan <fares.sultan9@gmail.com>
2024-09-27 20:08:37 +0300

    implementing Completed tasks page

--- Frontend/src/pages/CompletedTasks.jsx
+++ Frontend/src/pages/CompletedTasks.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Cards from '../components/cards';
+import axios from 'axios';
 const Completedtasks = () => {
+
+  const [Data,setData]=useState([]);
+ 
+  const headers= {id:localStorage.getItem("id"), authorization: `Bearer ${localStorage.getItem("token")}`}
+  useEffect(()=>{
+    const fetch = async (filter)=>{
+      const response= await axios.get(`http://localhost:8080/api/v1/tasks?filter=${filter}`, {headers,});
+      
+      setData(response.data.tasks);
+    };
+    fetch("completed"); 
+  },[])
   return (
    <div>
-        <Cards  home={"false"}/>
+        <Cards  home={"false"} data={Data}/>
    </div>
   );
 }
